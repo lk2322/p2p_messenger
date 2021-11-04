53b213e8b57ed90fd8e0e317011dc50453ef9dc2
lk2322 <lk2322@ya.ru>
2021-11-04 19:28:24 +0500

    Fix db

--- db/dals/users_dal.py
+++ db/dals/users_dal.py
@@ -16,8 +16,11 @@ class UserDAL():
         new_user = Users(name=name, ip=ip, hashed_private_key=hashed_private_key, hashed_public_key=hashed_public_key,
                          hashed_public_key_addr=hashed_public_key_addr)
         self.db_session.add(new_user)
-        await self.db_session.flush()
+        await self.db_session.commit()
+        await self.db_session.close()
+
 
     async def get_all_users(self) -> List[Users]:
         q = await self.db_session.execute(select(Users).order_by(Users.id))
+        await self.db_session.close()
         return q.scalars().all()

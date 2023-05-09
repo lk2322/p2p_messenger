5f354cc6d5a1a0ceecc095187a5c1d1b8ef2a561
lk2322 <lk2322@ya.ru>
2023-05-09 18:50:22 +0500

    Update dependency

--- network/logger.go
+++ network/logger.go
@@ -1,14 +1,18 @@
 package network
 
+import "log"
+
 type Logger struct {
 }
 
-func (l Logger) Info(_ string) {
+func (l Logger) Info(msg string) {
+	log.Println("INFO " + msg)
+}
+func (l Logger) Warn(msg string) {
+	log.Println("WARN " + msg)
 
 }
-func (l Logger) Warn(_ string) {
-
-}
-func (l Logger) Error(_ string) {
+func (l Logger) Error(msg string) {
+	log.Println("ERROR " + msg)
 
 }
